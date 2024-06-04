6f417dfdb6bc88692c114fe4d12d381e49cfde00
Samuli Toppi <samuli.toppi@outlook.com>
2024-06-04 16:18:13 +0300

    11.9 Simple end-to-end tests complete?

---
+++ cypress/e2e/first.cy.js
@@ -0,0 +1,14 @@
+describe('Pokedex', function() {
+  it('front page can be opened', function() {
+    cy.visit('http://localhost:8080')
+    cy.contains('ivysaur')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+
+  it('can navigate to Ivysaur page', function() {
+    cy.visit('http://localhost:8080')
+    cy.get('.list-item-name').contains('ivysaur').click()
+    cy.url().should('include', '/pokemon/ivysaur')
+    cy.contains('chlorophyll')
+  })
+})
