ca75768f9460727864f2f463e1797aa2304c0d09
Samuli Toppi <samuli.toppi@outlook.com>
2024-06-05 09:16:03 +0300

    11.9 Simple end-to-end tests complete? 6. try

--- playwright.config.js
+++ playwright.config.js
@@ -72,7 +72,7 @@ module.exports = defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run start',
-    url: 'http://192.168.0.100:8080',
+    url: 'http://192.168.100.17:8080',
     reuseExistingServer: !process.env.CI,
   },
 })
